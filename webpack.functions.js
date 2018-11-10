3de716a90376a69fcff84f12fc203b202c2c4d82
SoulShaker <triple56@yandex.ru>
2018-11-10 20:30:25 +0300

    feat: add custom webpack config

---
+++ webpack.functions.js
@@ -0,0 +1,13 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        use: {
+          loader: 'pug-loader',
+          query: {}
+        }
+      }
+    ]
+  }
+};
